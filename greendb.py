c27207c57385508d3cb9d9a456675e869588f897
Charles Leifer <coleifer@gmail.com>
2018-11-14 10:14:45 -0600

    0.1.3

--- greendb.py
+++ greendb.py
@@ -30,7 +30,7 @@ import struct
 import time
 
 
-__version__ = '0.1.2'
+__version__ = '0.1.3'
 
 logger = logging.getLogger(__name__)
 
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.md')) as fh:
 try:
     version = __import__('greendb').__version__
 except ImportError:
-    version = '0.1.2'
+    version = '0.1.3'
 
 
 setup(
