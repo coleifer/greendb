e9db55468813b001aa0d02d6ebef0382b0a3e9b4
Charles Leifer <coleifer@gmail.com>
2026-01-06 18:30:57 -0600

    Update build system.

--- setup.py
+++ setup.py
@@ -1,34 +1,6 @@
-import os
 from setuptools import setup
 
-
-with open(os.path.join(os.path.dirname(__file__), 'README.md')) as fh:
-    readme = fh.read()
-
-try:
-    version = __import__('greendb').__version__
-except ImportError:
-    version = '0.2.3'
-
-
-setup(
-    name='greendb',
-    version=version,
-    description='greendb',
-    long_description='greendb',
-    author='Charles Leifer',
-    author_email='coleifer@gmail.com',
-    url='http://github.com/coleifer/greendb/',
-    packages=[],
-    py_modules=['greendb', 'greenquery'],
-    install_requires=['lmdb', 'gevent', 'msgpack-python'],
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-    ],
-    scripts=['greendb.py'],
-    test_suite='tests')
+setup(name='greendb',
+      packages=[],
+      py_modules=['greendb', 'greenquery'],
+      install_requires=['lmdb', 'gevent', 'msgpack-python'])
